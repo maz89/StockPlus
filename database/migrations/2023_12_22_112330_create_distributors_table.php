8f35f897ab252ab5fdf5df2bdcfa6965c0e88054
Mazamesso <pmazama@gmail.com>
2024-01-04 17:26:12 +0000

    models and migrations

---
+++ database/migrations/2023_12_22_112330_create_distributors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('distributors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->bigInteger('owner_id');
+            $table->bigInteger('area_id');
+            $table->bigInteger('packaging_id');
+            $table->bigInteger('product_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('distributors');
+    }
+};
